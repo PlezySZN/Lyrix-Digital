abd66e2f721ee414d57b2ff15a0ed0f8e4c76876
PlezySZN <yomosantiago15@gmail.com>
2026-02-12 15:32:17 -0400

    fix: update Google Analytics integration for accurate engagement tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -117,7 +117,7 @@ export default defineConfig({
     }),
     partytown({
       config: {
-        forward: ['dataLayer.push'],
+        forward: ['dataLayer.push', 'gtag'],
       },
     }),
   ],
